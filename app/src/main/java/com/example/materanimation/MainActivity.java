167f2ea8426ae6db907307759639dc039a304813
shoaib332433 <shoaib.shah660@gmail.com>
2019-10-28 19:44:06 +0500

    Second commit

--- app/src/main/java/com/example/materanimation/MainActivity.java
+++ app/src/main/java/com/example/materanimation/MainActivity.java
@@ -10,29 +10,33 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView txt;
-    EditText enterno;
+    private TextView txt,txt2;
+    private EditText enterno;
+    private Boolean animation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         txt=findViewById(R.id.txt);
+        txt2=findViewById(R.id.txt2);
         enterno=findViewById(R.id.enterno);
-
 
 
         txt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-
-
-                float num=Float.parseFloat(enterno.getText().toString());
-                txt.animate().alpha(num).setDuration(500);
-
+                if (animation.equals(true)){
+                    txt.animate().alpha(0).setDuration(3000);
+                    txt2.animate().alpha(1).setDuration(3000);
+                    animation = false;
+                }else if (!animation.equals(true)){
+                    txt2.animate().alpha(0).setDuration(3000);
+                    txt.animate().alpha(1).setDuration(3000);
+                    animation = true;
+                }
             }
         });
 
